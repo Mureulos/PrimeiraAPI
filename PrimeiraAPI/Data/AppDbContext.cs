3b691387a760d1351f8a422485aba7db8f400d96
Murilo Jose <murilodesantana05@gmail.com>
2025-01-16 16:28:48 -0300

    Feat: Instalação do framework e criação do contexto de conexão com o DB

--- PrimeiraAPI/Data/AppDbContext.cs
+++ PrimeiraAPI/Data/AppDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace PrimeiraAPI.Data
-{
-    public class AppDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using PrimeiraAPI.Models;
 
+namespace PrimeiraAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base (options)
+        {   
+        }
+
+        public DbSet<AuthorModel> Authors { get; set; }
+        public DbSet<BookModel> Books { get; set; }
     }
 }
